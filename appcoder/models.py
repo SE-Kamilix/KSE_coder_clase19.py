045d0805bf0b652bec7dd4615d2647361ef310eb
Kamilo Santellán <kamilix.se@gmail.com>
2024-08-14 12:19:26 -0300

    first commit

---
+++ appcoder/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+
+    nombre = models.CharField(max_length=30)
+
+    comisión = models.IntegerField()
+
+class Estudiante(models.Model):
+
+    nombre = models.CharField(max_length=30)
+
+    apellido = models.CharField(max_length=30)
+
+    email = models.EmailField()
+
+    profesion = models.CharField(max_length=40, null=True)
+
+class Profesor(models.Model):
+
+    nombre = models.CharField(max_length=40)
+
+    apellido = models.CharField(max_length=40)
+
+    email = models.EmailField()
+
+    profesion = models.CharField(max_length=40)
+
+class Entregable(models.Model):
+
+    nombre = models.CharField(max_length=30)
+
+    fecha_de_entrega = models.DateField()
+
+    entregado = models.BooleanField()
